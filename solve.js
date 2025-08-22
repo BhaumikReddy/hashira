43c4a08330ba967f4ccaeb98d00f873b5d3a56eb
S V Sai Bhaumik Reddy <135725252+BhaumikReddy@users.noreply.github.com>
2025-08-22 11:15:28 +0530

    Update solve.js

--- solve.js
+++ solve.js
@@ -8,33 +8,23 @@ class PolynomialSecretSolver {
     }
 
     readInput(filename) {
-        try {
-            const data = JSON.parse(fs.readFileSync(filename, 'utf8'));
-            this.n = data.keys.n;
-            this.k = data.keys.k;
-            return data;
-        } catch (error) {
-            throw new Error(`Error reading file ${filename}: ${error.message}`);
-        }
+        const data = JSON.parse(fs.readFileSync(filename, 'utf8'));
+        this.n = data.keys.n;
+        this.k = data.keys.k;
+        return data;
     }
 
     decodeValue(value, base) {
-        const decoded = parseInt(value, base);
-        if (isNaN(decoded)) {
-            throw new Error(`Failed to decode value "${value}" in base ${base}`);
-        }
-        return decoded;
+        return parseInt(value, base);
     }
 
     extractPoints(data) {
         this.points = [];
-        
         for (let i = 1; i <= this.n; i++) {
             if (data[i]) {
                 const x = i;
                 const base = parseInt(data[i].base);
-                const encodedY = data[i].value;
-                const y = this.decodeValue(encodedY, base);
+                const y = this.decodeValue(data[i].value, base);
                 this.points.push({ x, y });
             }
         }
@@ -42,11 +32,9 @@ class PolynomialSecretSolver {
 
     lagrangeInterpolation(points) {
         let result = 0;
-        
         for (let i = 0; i < points.length; i++) {
             const xi = points[i].x;
             const yi = points[i].y;
-            
             let li = 1;
             for (let j = 0; j < points.length; j++) {
                 if (i !== j) {
@@ -54,18 +42,12 @@ class PolynomialSecretSolver {
                     li *= (0 - xj) / (xi - xj);
                 }
             }
-            
             result += yi * li;
         }
-        
         return Math.round(result);
     }
 
     solve() {
-        if (this.points.length < this.k) {
-            throw new Error(`Insufficient points: need ${this.k}, have ${this.points.length}`);
-        }
-        
         const selectedPoints = this.points.slice(0, this.k);
         return this.lagrangeInterpolation(selectedPoints);
     }
@@ -80,16 +62,9 @@ class PolynomialSecretSolver {
 function main() {
     const args = process.argv.slice(2);
     const filename = args[0] || 'testcase1.json';
-    
     const solver = new PolynomialSecretSolver();
-    
-    try {
-        const secret = solver.solveProblem(filename);
-        console.log(`Secret: ${secret}`);
-    } catch (error) {
-        console.error('Error:', error.message);
-        process.exit(1);
-    }
+    const secret = solver.solveProblem(filename);
+    console.log(`Secret: ${secret}`);
 }
 
 if (require.main === module) {
